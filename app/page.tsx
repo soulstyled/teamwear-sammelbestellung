44a5443f6af82f407d63391b8d118d95afb7350b
Michael Geißler <michael.geissler@dehner.de>
2025-02-25 14:58:14 +0100

    Minor Typo

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
     <main className="container mx-auto p-6 max-w-screen-md">
       <h1 className="text-3xl font-semibold mb-6">TuS Feuchtwangen Teamwear Sammelbestellung</h1>
       <ul className="mb-5 list-disc pl-8">
-        <li><span className="font-medium">Lieferzeit</span>: Je nach Verfügbarkeit ca. 2-3 Wochen</li>
+        <li><span className="font-medium">Lieferzeit</span>: je nach Verfügbarkeit ca. 2-3 Wochen</li>
         <li><span className="font-medium">Personalisierung</span>: Initialen und Nummer möglich, maximal 4 Zeichen. Aufpreis: 3,00 €</li>
-        <li><span className="font-medium">Bezahlung</span>: Bar oder per PayPal bei Übergabe</li>
-        <li><span className="font-medium">Lieferung</span>: Die Übergabe erfolgt beim Jugendtraining</li>
-        <li><span className="font-medium">Retoure</span>: Ausgeschlossen, da personalisierte Ware</li>
+        <li><span className="font-medium">Bezahlung</span>: bar oder per PayPal bei Übergabe</li>
+        <li><span className="font-medium">Lieferung</span>: die Übergabe der fertigen Klamotten erfolgt beim Training</li>
+        <li><span className="font-medium">Retoure</span>: ausgeschlossen, da personalisierte Ware</li>
       </ul>
   <p className="mb-5">Bei Fragen an Michael Geißler wenden (<a href="tel:+491783137341">0178 / 3137341</a> oder <a href="mailto:geisslersmichi@gmail.com">geisslersmichi@gmail.com</a>)</p>
       <hr className="pb-12"/>
